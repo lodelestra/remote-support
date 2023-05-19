1d3d9887f89835e1a99dffc10fbbc1fbaf0609f8
Loïc Delestra <loic+github@delestra.com>
2023-05-20 01:11:15 +0200

    Allow to list and add user's websites

--- components/FormAddWebsite.tsx
+++ components/FormAddWebsite.tsx
@@ -1,7 +1,10 @@
+import { useRef } from 'react';
 import { useUser } from '@supabase/auth-helpers-react';
 
-export default function FormAddWebsite({callback: callback}: {callback: Function}) {
-  const  userDetails  = useUser()
+export default function FormAddWebsite({ callback: callback }: { callback: Function }) {
+  const userDetails = useUser()
+
+  const ref = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault()
@@ -17,16 +20,19 @@ export default function FormAddWebsite({callback: callback}: {callback: Function
       user_id: userDetails?.id
     }
 
-    await fetch('/api/create-user-website', {method: "POST", body: JSON.stringify(data)})
+    await fetch('/api/create-user-website', { method: "POST", body: JSON.stringify(data) })
+    if (ref.current !== null) {
+      ref.current.value = ''
+    }
     callback()
   }
-  
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="url">Url</label>
-      <input type="text" className="bg-gray-800" id="url" name="url" required />
 
-      <button type="submit">Submit</button>
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-row space-x-4">
+      <label htmlFor="url" className="hidden">Url</label>
+      <input ref={ref} type="text" className="bg-gray-800 grow" id="url" name="url" placeholder='URL' required />
+
+      <button type="submit" className="btn btn-green">Add website</button>
     </form>
   )
 }
--- pages/websites.tsx
+++ pages/websites.tsx
@@ -37,13 +37,11 @@ async function loadData(setWebsites: Function, supabaseClient: SupabaseClient) {
 
 export default function Page({ userWebsites }: { userWebsites: Array<Website> }) {
   const supabaseClient = useSupabaseClient()
-  // const { userDetails } = useUser()
   const userDetails = useUser()
   const [websites, setWebsites] = useState<Website[] | null>(null)
 
   const loadDataClosure = () => { loadData(setWebsites, supabaseClient) }
 
-  // const user = useContext(UserContext)
 
   useEffect(() => {
     if (userDetails) {
@@ -53,19 +51,76 @@ export default function Page({ userWebsites }: { userWebsites: Array<Website> })
 
 
   if (websites) {
-    const userWebsites = websites.map(ws => {
-      return <WebsiteItem key={ws.id} onWebsiteDelete={() => { handleDeleteWebsite(supabaseClient, ws.id, loadDataClosure) }} {...ws} />
-    })
+    const websitesRows =
+      websites.map((website) => (
+        <tr key={website.id}>
+          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+            {website.url}
+          </td>
+          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{website.public_key}</td>
+          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+            <a href="#" className="text-indigo-400 hover:text-indigo-300">
+              Edit<span className="sr-only">, {website.public_key}</span>
+            </a>
+            <br />
+            <a href="#" onClick={() => { handleDeleteWebsite(supabaseClient, website.id, loadDataClosure) }} className="text-indigo-400 hover:text-indigo-300">
+              Delete<span className="sr-only">, {website.public_key}</span>
+            </a>
+          </td>
+        </tr>
+      ))
 
     return (
       <>
-        <h3>
-          list sites
-        </h3>
-        <ul className="gap-">
-          {userWebsites}
-        </ul>
-        <FormAddWebsite callback={loadDataClosure} />
+        <div className="bg-zinc-900">
+          <div className="mx-auto max-w-7xl">
+            <div className="bg-zinc-900 py-10">
+              <div className="px-4 sm:px-6 lg:px-8">
+                <div className="sm:flex sm:items-center">
+                  <div className="sm:flex-auto">
+                    <h1 className="text-base font-semibold leading-6 text-white">Websites</h1>
+                    <p className="mt-2 text-sm text-gray-300">
+                      A list of all the websites managed by your support account
+                    </p>
+                  </div>
+                  <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+
+                  </div>
+                </div>
+                <div className="mt-8 flow-root">
+                  <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+
+                      <table className="min-w-full divide-y divide-gray-700">
+
+                        <thead>
+                          <tr>
+                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">
+                              URL
+                            </th>
+                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
+                              Public key
+                            </th>
+                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-white">
+                              Number of sessions
+                            </th>
+                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                              <span className="sr-only">Edit</span>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-800">
+                          {websitesRows}
+                        </tbody>
+                      </table>
+                      <FormAddWebsite callback={loadDataClosure} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
